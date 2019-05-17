209edbd2d9c508b967405379996e3121b78e90c7
widianapw <widianaputraa@gmail.com>
2019-05-17 09:48:11 +0800

    fix

--- db_con.php
+++ db_con.php
@@ -1,5 +1,5 @@
 <?php
-	$koneksi =  mysqli_connect("remotemysql.com","pYRyUKA0J1","TYeoeqCy3h","pYRyUKA0J1");
+	$koneksi =  mysqli_connect("www.db4free.net","captainhook","widiana1999","captainhook");
 	if(mysqli_connect_errno()){
 		printf ("Gagal terkoneksi : ".mysqli_connect_error());
 		exit();
--- input.php
+++ input.php
@@ -25,105 +25,58 @@ function process_message($message)
 		$text = $message_data["text"];
 		mysqli_query($koneksi, "INSERT INTO tb_inbox VALUES (null,'$chatid','$text','msg','1',NOW())");
 
-		$fetch =  mysqli_query($koneksi, "SELECT MAX(id_inbox) as id_inbox FROM tb_inbox");
-		$id = mysqli_fetch_assoc($fetch);
-		$id_inbox =  $id["id_inbox"];
-		$msg = mysqli_query($koneksi, "INSERT INTO tb_outbox VALUES (null,'$id_inbox','$chatid','$text','msg','1', NOW())");
-		if ($msg) {
-			mysqli_query($koneksi, "UPDATE tb_inbox set flag = '2' where id_inbox = $id_inbox ");	
-		}	
 	}
 
 	elseif (isset($message_data["photo"])) {
 		$chatid = $message_data["chat"]["id"];
 		$photo_id = $message_data["photo"]["0"]["file_id"];
 		mysqli_query($koneksi, "INSERT INTO tb_inbox VALUES (null,'$chatid','$photo_id','img','1',NOW())");
-		$fetch =  mysqli_query($koneksi, "SELECT MAX(id_inbox) as id_inbox FROM tb_inbox");
-		$id = mysqli_fetch_assoc($fetch);
-		$id_inbox =  $id["id_inbox"];
-		mysqli_query($koneksi, "INSERT INTO tb_outbox VALUES (null,'$id_inbox','$chatid','$photo_id','img','1', NOW())");
-		mysqli_query($koneksi, "UPDATE tb_inbox set flag = '2' where id_inbox = $id_inbox ");
 
 	}
 	elseif (isset($message_data["location"])) {
 		$chatid = $message_data["chat"]["id"];
 		mysqli_query($koneksi, "INSERT INTO tb_inbox VALUES (null,'$chatid','location','loc','1',NOW())");
-		$fetch =  mysqli_query($koneksi, "SELECT MAX(id_inbox) as id_inbox FROM tb_inbox");
-		$id = mysqli_fetch_assoc($fetch);
-		$id_inbox =  $id["id_inbox"];
-		echo $chatid;
-		mysqli_query($koneksi, "INSERT INTO tb_outbox VALUES (null,'$id_inbox','$chatid','https://goo.gl/maps/5Nu9WB2tbzvNRTEn9','loc','1', NOW())");
-		mysqli_query($koneksi, "UPDATE tb_inbox set flag = '2' where id_inbox = $id_inbox ");
+		
 	}
 	elseif (isset($message_data["document"])) {
 		$chatid = $message_data["chat"]["id"];
 		$file_name = $message_data["document"]["file_name"];
 		mysqli_query($koneksi, "INSERT INTO tb_inbox VALUES (null,'$chatid','$file_name','file','1',NOW())");
-		$fetch =  mysqli_query($koneksi, "SELECT MAX(id_inbox) as id_inbox FROM tb_inbox");
-		$id = mysqli_fetch_assoc($fetch);
-		$id_inbox =  $id["id_inbox"];
-		echo $chatid;
-		mysqli_query($koneksi, "INSERT INTO tb_outbox VALUES (null,'$id_inbox','$chatid','http://ikd.ugm.ac.id/files/download/CV.doc','file','1', NOW())");
-		mysqli_query($koneksi, "UPDATE tb_inbox set flag = '2' where id_inbox = $id_inbox ");	
+		
 	}
 
 	elseif (isset($message_data["sticker"])) {
 		$chatid = $message_data["chat"]["id"];
 		$file_id = $message_data["sticker"]["file_id"];
 		mysqli_query($koneksi, "INSERT INTO tb_inbox VALUES (null,'$chatid','$file_id','stc','1',NOW())");
-		$fetch =  mysqli_query($koneksi, "SELECT MAX(id_inbox) as id_inbox FROM tb_inbox");
-		$id = mysqli_fetch_assoc($fetch);
-		$id_inbox =  $id["id_inbox"];
-		echo $chatid;
-		mysqli_query($koneksi, "INSERT INTO tb_outbox VALUES (null,'$id_inbox','$chatid','$file_id','stc','1', NOW())");
-		mysqli_query($koneksi, "UPDATE tb_inbox set flag = '2' where id_inbox = $id_inbox");	
+		
 	}
 
 	elseif (isset($message_data["contact"])) {
 		$chatid = $message_data["chat"]["id"];
 		$first_name = $message_data["contact"]["first_name"];
 		mysqli_query($koneksi, "INSERT INTO tb_inbox VALUES (null,'$chatid','$first_name','ctc','1',NOW())");
-		$fetch =  mysqli_query($koneksi, "SELECT MAX(id_inbox) as id_inbox FROM tb_inbox");
-		$id = mysqli_fetch_assoc($fetch);
-		$id_inbox =  $id["id_inbox"];
-		echo $chatid;
-		mysqli_query($koneksi, "INSERT INTO tb_outbox VALUES (null,'$id_inbox','$chatid','$first_name','ctc','1', NOW())");
-		mysqli_query($koneksi, "UPDATE tb_inbox set flag = '2' where id_inbox = $id_inbox");	
+		
 	}
 
 	elseif (isset($message_data["video"])) {
 		$chatid = $message_data["chat"]["id"];
 		$file_id = $message_data["video"]["file_id"];
 		mysqli_query($koneksi, "INSERT INTO tb_inbox VALUES (null,'$chatid','$file_id','vdo','1',NOW())");
-		$fetch =  mysqli_query($koneksi, "SELECT MAX(id_inbox) as id_inbox FROM tb_inbox");
-		$id = mysqli_fetch_assoc($fetch);
-		$id_inbox =  $id["id_inbox"];
-		echo $chatid;
-		mysqli_query($koneksi, "INSERT INTO tb_outbox VALUES (null,'$id_inbox','$chatid','$file_id','vdo','1', NOW())");
-		mysqli_query($koneksi, "UPDATE tb_inbox set flag = '2' where id_inbox = $id_inbox");	
+		
 	}
 
 	elseif (isset($message_data["voice"])) {
 		$chatid = $message_data["chat"]["id"];
 		$file_id = $message_data["voice"]["file_id"];
 		mysqli_query($koneksi, "INSERT INTO tb_inbox VALUES (null,'$chatid','$file_id','vic','1',NOW())");
-		$fetch =  mysqli_query($koneksi, "SELECT MAX(id_inbox) as id_inbox FROM tb_inbox");
-		$id = mysqli_fetch_assoc($fetch);
-		$id_inbox =  $id["id_inbox"];
-		echo $chatid;
-		mysqli_query($koneksi, "INSERT INTO tb_outbox VALUES (null,'$id_inbox','$chatid','$file_id','vic','1', NOW())");
-		mysqli_query($koneksi, "UPDATE tb_inbox set flag = '2' where id_inbox = $id_inbox");	
+		
 	}
 
 	else{
 		$chatid = $message_data["chat"]["id"];
 		mysqli_query($koneksi, "INSERT INTO tb_inbox VALUES (null,'$chatid','format belum diketahui','unk','1',NOW())");
-		$fetch =  mysqli_query($koneksi, "SELECT MAX(id_inbox) as id_inbox FROM tb_inbox");
-		$id = mysqli_fetch_assoc($fetch);
-		$id_inbox =  $id["id_inbox"];
-		echo $chatid;
-		mysqli_query($koneksi, "INSERT INTO tb_outbox VALUES (null,'$id_inbox','$chatid','format belum diketahui','unk','1', NOW())");
-		mysqli_query($koneksi, "UPDATE tb_inbox set flag = '2' where id_inbox = $id_inbox");
+		
 	}
 
 	return $updateid;
